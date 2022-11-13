4467f0ad9df2b636651ad8423e297c1a00dd0d87
mrrmainly <ernest_everstov@mail.ru>
2022-11-13 18:35:11 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import WheelComponent from "react-wheel-of-prizes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const segments = ["better luck next time", "won 70", "won 10", "better luck next time", "won 2", "won uber pass", "better luck next time", "won a voucher"];
+    const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F", "#F9AA1F", "#EC3F3F", "#FF9000"];
+    const onFinished = (winner) => {
+        console.log(winner);
+    };
+    return (
+        <div className="App">
+            <WheelComponent
+                segments={segments}
+                segColors={segColors}
+                winningSegment="won 10"
+                onFinished={(winner) => onFinished(winner)}
+                primaryColor="black"
+                contrastColor="white"
+                buttonText="К"
+                isOnlyOnce={false}
+                size={290}
+                upDuration={100}
+                downDuration={10000}
+                fontFamily="Arial"
+            />
+        </div>
+    );
 }
 
 export default App;
